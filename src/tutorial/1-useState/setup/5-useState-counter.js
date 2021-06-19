440ea2ae17e578502dedc97ccf35e70d31921471
irah rosete <irah.rosete@outlook.com>
2021-06-19 19:46:25 +1000

    complete complex counter using callback function

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,12 +7,28 @@ const UseStateCounter = () => {
     setValue(0)
   }
 
+  const complexCounter = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevCount) => {
+        return prevCount + 1
+      })
+    }, 2000)
+  }
+
   return <>
-    <h3>Counter</h3>
-    <h1>{value}</h1>
-    <button className="btn" onClick={() => setValue(value - 1)}>-</button>
-    <button className="btn" onClick={reset}>reset</button>
-    <button className="btn" onClick={() => setValue(value + 1)}>+</button>
+    <section style={{margin: '4rem 0'}}>
+      <h3>Normal Counter</h3>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value - 1)}>-</button>
+      <button className="btn" onClick={reset}>reset</button>
+      <button className="btn" onClick={() => setValue(value + 1)}>+</button>
+    </section>
+    <section style={{margin: '4rem 0'}}>
+      <h3>Complex Counter</h3>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complexCounter}>increase</button>
+    </section>
   </>
 };
 
