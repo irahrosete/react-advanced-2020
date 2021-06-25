cedd20d3e09d1c6d4376079c36957fb6b95d0b46
irah rosete <irah.rosete@outlook.com>
2021-06-25 16:17:01 +1000

    setup useState for useReducer

--- README.md
+++ README.md
@@ -33,3 +33,5 @@
 14. Cart
 
 #### React Router
+
+15. Cocktails
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,46 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [modal, setModal] = useState(false)
 
-export default Index;
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      setModal(true)
+      setPeople([...people, { id: new Date().getTime().toString(), name }])
+      setName('')
+    } else {
+      setModal(true)
+    }
+  }
+
+  return (
+    <>
+      {modal && <Modal />}
+      <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={name} onChange={handleChange} />
+          <button>add</button>
+        </form>
+      </div>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
